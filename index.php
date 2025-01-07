4f430fafd3cedea787d5116173ef7ce4869e364d
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2025-01-07 21:28:21 +0000

    me learning php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+echo "Hello, hi <br> ";
+echo "Hello, hi<br>  ";
+
+$name = "hugo";
+$lastName = "pereira8814";
+
+$age = 21;
+$users = 2;
+
+echo "hi {$name} , {$lastName} <br>";
+echo "<br> $age <br>";
+echo "<br> $age <br>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>Click</button>
+</body>
+</html>
